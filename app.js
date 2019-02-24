05b9aad3dab8784db6b57b279f9a987db0c04fdc
srishtigoel27 <srishtigoel27@gmail.com>
2019-02-24 17:29:52 +0530

    adding function to find result

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 turn=1;
+fin1=0;
+fin2=0;
 sum1=0;
 sum2=0;
-check=0;
   function rolldice()
   {
     var max=6;
@@ -15,10 +16,12 @@ check=0;
         sum1=0;
         g=0;
       }
-      else {
+      else
+       {
         sum2=0;
         g=0;
       }
+      changeturn();
     }
     else if(g==2)
     {
@@ -41,31 +44,33 @@ check=0;
       document.getElementById("dice").src="dice-6.png";
     }
     addSum(g);
-    if(check==0)
-      changeturn();
-  }
-  function setcheck()
-  {
-    changeturn();
-    check=check+1;
-
   }
   function changeturn()
   {
     if(turn==1)
     {
       turn=2;
+      fin1=fin1+sum1;
+      sum1=0;
+      document.getElementById("score-0").innerHTML=fin1;
+      result(fin1,0);
       var h=document.getElementById("player-0-panel");
       h.className="";
       var d=document.getElementById("player-1-panel");
       d.className="active";
+      document.getElementById("current-0").innerHTML=0;
     }
     else {
       turn=1;
+      fin2=fin2+sum2;
+      sum2=0;
+      document.getElementById("score-1").innerHTML=fin2;
+      result(fin2,1);
       var h=document.getElementById("player-1-panel");
       h.className="";
       var d=document.getElementById("player-0-panel");
       d.className="active";
+      document.getElementById("current-1").innerHTML=0;
     }
   }
   function addSum(g)
@@ -73,11 +78,27 @@ check=0;
     if(turn==1)
     {
       sum1=sum1+g;
-      document.getElementById("score-0").innerHTML=sum1;
+      document.getElementById("current-0").innerHTML=sum1;
     }
     else
     {
       sum2=sum2+g;
-      document.getElementById("score-1").innerHTML=sum2;
+      document.getElementById("current-1").innerHTML=sum2;
     }
+  }
+  function result(f,a)
+  {
+    if(f>=100)
+    {
+      if(a==0)
+      {
+        document.getElementById("name-0").innerHTML="WINNER";
+      }
+      else
+       {
+        document.getElementById("name-1").innerHTML="WINNER";
+      }
+    document.querySelector(".btn-roll").disabled="true";
+    document.querySelector(".btn-hold").disabled="true";
+  }
   }
