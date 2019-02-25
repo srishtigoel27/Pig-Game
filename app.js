ae2abd7b2e47c9a7c9817279ac83193fffbdcd2b
srishtigoel27 <srishtigoel27@gmail.com>
2019-02-25 07:20:53 +0530

    adding 2 dice game

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ fin1=0;
 fin2=0;
 sum1=0;
 sum2=0;
-  function rolldice()
+ch=1;
+  function rolldice(d)
   {
-    var max=6;
-    var min=1;
-    g =Math.floor(Math.random() * (+max - +min)) + +min;
+    console.log(ch);
+    g =Math.floor(Math.random()*6)+1;
     if(g==1)
     {
-      document.getElementById("dice").src="dice-1.png";
+      document.getElementById(d).src="dice-1.png";
       if(turn==1)
       {
         sum1=0;
@@ -21,29 +21,35 @@ sum2=0;
         sum2=0;
         g=0;
       }
-      changeturn();
+      if(ch!=0)
+        changeturn();
     }
     else if(g==2)
     {
-      document.getElementById("dice").src="dice-2.png";
+      document.getElementById(d).src="dice-2.png";
     }
     else if(g==3)
     {
-      document.getElementById("dice").src="dice-3.png";
+      document.getElementById(d).src="dice-3.png";
     }
     else if(g==4)
     {
-      document.getElementById("dice").src="dice-4.png";
+      document.getElementById(d).src="dice-4.png";
     }
     else if(g==5)
     {
-      document.getElementById("dice").src="dice-5.png";
+      document.getElementById(d).src="dice-5.png";
     }
     else if(g==6)
     {
-      document.getElementById("dice").src="dice-6.png";
+      document.getElementById(d).src="dice-6.png";
     }
     addSum(g);
+    if(d=='dice')
+      ch=g;
+    else {
+      ch=1;
+    }
   }
   function changeturn()
   {
@@ -98,8 +104,10 @@ sum2=0;
        {
         document.getElementById("name-1").innerHTML="WINNER";
       }
-    document.querySelector(".btn-roll").disabled="true";
-    document.querySelector(".btn-hold").disabled="true";
+    document.querySelector(".btn-roll").disabled=true;
+    document.querySelector(".btn-hold").disabled=true;
+    document.getElementById("dice").src="";
+    document.getElementById("dicetwo").src="";
   }
   }
   function newgame()
@@ -108,10 +116,19 @@ sum2=0;
     sum2=0;
     fin1=0;
     fin2=0;
+    document.querySelector(".btn-roll").disabled=false;
+    document.querySelector(".btn-hold").disabled=false;
     document.getElementById("name-1").innerHTML="Player 2";
     document.getElementById("name-0").innerHTML="Player 1";
     document.getElementById("score-1").innerHTML=0;
     document.getElementById("score-0").innerHTML=0;
     document.getElementById("current-0").innerHTML=0;
     document.getElementById("current-1").innerHTML=0;
+    var h=document.getElementById("player-1-panel");
+    h.className="";
+    var d=document.getElementById("player-0-panel");
+    d.className="active";
+    turn=1;
+    document.getElementById("dice").src="dice-1.png";
+    document.getElementById("dicetwo").src="dice-1.png";
   }
